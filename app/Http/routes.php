7794fd15d89ab3fbe455cc63ea2e01ce96676255
jcj <jcj@qq.com>
2017-01-08 19:27:25 +0800

    1.8号提交初始化模板

---
+++ app/Http/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//定义一个后台登录验证组
+Route::group(['middleware'=>'adminlogin'],function(){
+	//后台首页
+	Route::get('/admin','admin\IndexController@index');
+	//管理员
+	Route::controller('/admin/users_admin','admin\Users_adminController');
+	//用户
+	Route::controller('/admin/users','admin\UsersController');
+	//分类
+	Route::controller('/admin/cate','admin\cateController');
+	//用户浏览路由
+	Route::controller('admin/type','admin\typeController');
+	//店铺浏览
+	Route::controller('admin/shop','admin\shopController');
+	//商品首页
+	Route::controller('admin/goods','admin\GoodsController');
+});
+//后台登录页面
+Route::controller('admin/login','admin\loginController');
